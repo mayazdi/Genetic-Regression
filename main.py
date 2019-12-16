06f56387dc05a36ba66f71a9c7cfd4460680c2a7
Amin <yazdi.mohammad99@gmail.com>
2019-12-17 01:28:39 +0330

    Finalized

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import operator
+# Code Available at https://github.com/mayazdi/Genetic-Regression
 import random
 import struct
 
@@ -24,12 +24,11 @@ class CoEf:
         self.fitness = fitness_calc(a, b, c)
 
 
-def init():
-    for i in range(0, genome):
-        a = random.uniform(-50, 50)
-        b = random.uniform(-50, 50)
-        c = random.uniform(-50, 50)
-        population.append(CoEf(a, b, c))
+def fitness_calc(a, b, c):
+    diff = 0
+    for i in range(0, 999):
+        diff += abs(yCos[i] - poly(xCos[i], a, b, c))
+    return diff
 
 
 def poly(x, a, b, c):
@@ -37,9 +36,6 @@ def poly(x, a, b, c):
 
 
 def draw(coef):
-    # for x in range(-100, 100, 1):
-    # fig = plt.figure()
-    # axes = fig.add_subplot(111)
     v = numpy.arange(-100, 100, 1)
     plt.plot(v, poly(v, coef.a, coef.b, coef.c))
     plt.show()
@@ -54,22 +50,24 @@ def report(individual):
     print("-----------------------------------------------")
 
 
-def fitness_calc(a, b, c):
-    diff = 0
-    for i in range(0, 999):
-        diff += abs(yCos[i] - poly(xCos[i], a, b, c))
-    return diff
+# Initial Genomes
+def init():
+    for i in range(0, genome):
+        a = random.uniform(-50, 50)
+        b = random.uniform(-50, 50)
+        c = random.uniform(-50, 50)
+        population.append(CoEf(a, b, c))
 
 
 # Cut-Off half with low fitness
 def selection():
     population.sort(key=lambda x: x.fitness, reverse=True)
-    # population.sort(key=lambda x: x.fitness, reverse=True)
     size = int(len(population) / 2)
     for i in range(0, size):
         population.remove(population[0])
 
 
+# Fetch random Index
 def findIndex(size):
     rand_probability = random.randint(1, 100)
     if rand_probability <= 33:
@@ -79,15 +77,15 @@ def findIndex(size):
     return index
 
 
+# Cross two A or B or C of CoEf[ind1] & CoEf[ind2]
 def cross(num1, num2):
     index = random.randint(0, 31)
-    child1 = float_to_bin(num1)[0:index] + float_to_bin(num2)[index:len(float_to_bin(num1))]
-    child2 = float_to_bin(num2)[0:index] + float_to_bin(num1)[index:len(float_to_bin(num1))]
-
-    while (not (-50 <= float(bin_to_float(child1)) <= 50)) or (not (-50 <= float(bin_to_float(child2)) <= 50)):
+    child1 = float_to_bin(num1)[0:index] + float_to_bin(num2)[index:]
+    child2 = float_to_bin(num2)[0:index] + float_to_bin(num1)[index:]
+    while not ((-50 <= float(bin_to_float(child1)) <= 50) and (-50 <= float(bin_to_float(child2)) <= 50)):
         index = random.randint(0, 31)
-        child1 = float_to_bin(num1)[0:index] + float_to_bin(num2)[index:len(float_to_bin(num1))]
-        child2 = float_to_bin(num2)[0:index] + float_to_bin(num1)[index:len(float_to_bin(num1))]
+        child1 = float_to_bin(num1)[0:index] + float_to_bin(num2)[index:]
+        child2 = float_to_bin(num2)[0:index] + float_to_bin(num1)[index:]
     li = [bin_to_float(child1), bin_to_float(child2)]
     return li
 
@@ -107,11 +105,20 @@ def crossover():
         population.append(addition[i])
 
 
+def switch(num):
+    if num == 0:
+        return 1
+    else:
+        return 0
+
+
+# Mute A, B and C of CoEf's
 def mute(num):
+    # Float is 32 Bits, So we decide for the chosen bit next line!
     index = random.randint(0, 31)
     ind = num
     ind = float_to_bin(ind)
-    ind = ind[0:index] + str(random.randint(0, 1)) + ind[index + 1:len(ind)]
+    ind = ind[0:index] + str(switch(ind[index])) + ind[index + 1:len(ind)]
     while not (-50 <= float(bin_to_float(ind)) <= 50):
         index = random.randint(0, 31)
         ind = num
@@ -132,6 +139,7 @@ def mutate():
             population[i] = CoEf(A, B, C)
 
 
+# Code Initiate here
 xCos = numpy.genfromtxt('x_train.csv', delimiter=',')
 yCos = numpy.genfromtxt('y_train.csv', delimiter=',')
 population = list()
